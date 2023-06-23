a9fe01c812e808d9929e67e37fd5282543aaa6a8
Atanu Sarkar <mratanusarkar@gmail.com>
2023-06-23 16:38:10 +0530

    add: more mouse events

--- vue-basics/app.js
+++ vue-basics/app.js
@@ -8,7 +8,9 @@ const app = Vue.createApp({
             author: "Douglas Adams",
             year: 1996,
             hits: 0,
-            click_counter: 0
+            click_counter: 0,
+            mouse_x: 0,
+            mouse_y: 0
         }
     },
     methods: {
@@ -18,6 +20,13 @@ const app = Vue.createApp({
         divOnClick(step) {
             console.log("using @click, the div was clicked!", this.click_counter);
             this.click_counter += step
+        },
+        handleMouseEvent(event, data) {
+            console.log("box:", data, "> mouse event:", event.type);
+        },
+        handleMouseMove(event) {
+            this.mouse_x = event.offsetX;
+            this.mouse_y = event.offsetY;
         }
     }
 })
