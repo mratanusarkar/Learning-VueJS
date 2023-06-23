0e944efed9cd6a01172880c6526eebe2e1c99d0c
Atanu Sarkar <mratanusarkar@gmail.com>
2023-06-23 15:55:40 +0530

    add: conditional rendering
    - v-if and v-else: adds and removes html from dom
    - v-show: changes css (display: none) to hide or show, so it's fast

--- vue-basics/app.js
+++ vue-basics/app.js
@@ -3,6 +3,7 @@ console.log("This is a demo of a basic vue standalone widget, using vue CDN!");
 const app = Vue.createApp({
     data() {
         return {
+            showBooks: true,
             book: "The Hitchhiker's Guide to the Galaxy",
             author: "Douglas Adams",
             year: 1996,
@@ -11,6 +12,9 @@ const app = Vue.createApp({
         }
     },
     methods: {
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
+        },
         divOnClick(step) {
             console.log("using @click, the div was clicked!", this.click_counter);
             this.click_counter += step
