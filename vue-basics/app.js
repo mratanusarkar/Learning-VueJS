03b334d2fae98f0d82c6deafae83a172d697648e
Atanu Sarkar <mratanusarkar@gmail.com>
2023-06-26 11:33:27 +0530

    add: attribute binding
    - v-bind: for binding a js to a html attribute
    - note: shortcut of the same is simply using ":"

--- vue-basics/app.js
+++ vue-basics/app.js
@@ -5,16 +5,43 @@ const app = Vue.createApp({
         return {
             showBooks: true,
             books: [
-                { title: "The Hitchhiker's Guide to the Galaxy", author: "Douglas Adams", year: 1996 },
-                { title: "The Name of the Wind", author: "Patrick Rothfuss", year: 2007 },
-                { title: "The Way of Kings", author: "Brandon Sanderson", year: 2010 },
-                { title: "The Three-Body Problem", author: "Liu Cixin", year: 2008 },
-                { title: "Children of Time", author: "Adrian Tchaikovsky", year: 2013 }
+                {
+                    title: "The Hitchhiker's Guide to the Galaxy",
+                    author: "Douglas Adams",
+                    year: 1996,
+                    cover_img: "https://covers.openlibrary.org/b/id/12986869-M.jpg"
+                },
+                {
+                    title: "The Name of the Wind",
+                    author: "Patrick Rothfuss",
+                    year: 2007,
+                    cover_img: "https://covers.openlibrary.org/b/id/12391248-M.jpg"
+                },
+                {
+                    title: "The Way of Kings",
+                    author: "Brandon Sanderson",
+                    year: 2010,
+                    cover_img: "https://covers.openlibrary.org/b/id/7429768-M.jpg"
+                },
+                {
+                    title: "The Three-Body Problem",
+                    author: "Liu Cixin",
+                    year: 2008,
+                    cover_img: "https://covers.openlibrary.org/b/id/9157544-M.jpg"
+                },
+                {
+                    title: "Children of Time",
+                    author: "Adrian Tchaikovsky",
+                    year: 2013,
+                    cover_img: "https://covers.openlibrary.org/b/id/10157457-M.jpg"
+                },
             ],
             hits: 0,
             click_counter: 0,
             mouse_x: 0,
-            mouse_y: 0
+            mouse_y: 0,
+            src_url: "https://github.com/mratanusarkar/Learning-VueJS",
+            credit_url: "https://netninja.dev/"
         }
     },
     methods: {
