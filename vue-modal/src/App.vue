c0da9b2c2a4d1c8b00cfd9bf4ca0e897abebad1c
Atanu Sarkar <mratanusarkar@gmail.com>
2023-07-03 18:58:33 +0530

    add: template refs

--- vue-modal/src/App.vue
+++ vue-modal/src/App.vue
@@ -1,6 +1,10 @@
 <template>
+  <!-- Vue 3 now allows us to have multiple elements inside the root template -->
   <img alt="Vue logo" src="./assets/logo.png">
   <h1>{{ title }}</h1>
+  <input type="text" ref="name">
+  <button @click="handleButtonClick">click me</button>
+  <h3 @click="handleH3Click">Last Entry: {{ data }}</h3>
 </template>
 
 <script>
@@ -8,7 +12,28 @@ export default {
   name: 'App',
   data() {
     return {
-      title: "Welcome to My First Vue App!"
+      title: "Welcome to My First Vue App!",
+      data: "null"
+    }
+  },
+  methods: {
+    handleButtonClick() {
+      // access the DOM Element via $ref in contrary to the DOM query selector
+      console.log(this.$refs.name);
+      this.data = this.$refs.name.value ? this.$refs.name.value : "null"
+      
+      // add, remove, inject classes dynamically
+      if (this.$refs.name.value) {
+        this.$refs.name.classList.remove('fail')
+        this.$refs.name.classList.add('pass')
+      } else {
+        this.$refs.name.classList.remove('pass')
+        this.$refs.name.classList.add('fail')
+      }
+    },
+    handleH3Click() {
+      // we can also access other normal javascript methods & functions
+      this.$refs.name.focus()
     }
   }
 }
@@ -28,5 +53,11 @@ h1 {
   border-bottom: 1px solid #ddd;
   padding-top: 10px;
   padding-bottom: 10px;
+}
+.pass {
+  border: 2px solid green;
+}
+.fail {
+  border: 2px solid red;
 }
 </style>
