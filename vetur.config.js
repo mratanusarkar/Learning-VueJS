ce849c568f9fc0dcdfcff963c0139b4fc427c980
Atanu Sarkar <mratanusarkar@gmail.com>
2023-07-03 11:39:39 +0530

    chore: disable vetur warnings and errors
    source:
    - https://vuejs.github.io/vetur/guide/setup.html#advanced
    - https://stackoverflow.com/questions/64867504/vue-3-the-template-root-requires-exactly-one-element-eslint-plugin-vue

---
+++ vetur.config.js
@@ -0,0 +1,11 @@
+/** @type {import('vls').VeturConfig} */
+module.exports = {
+    projects: [
+        './vue-modal'
+    ]
+}
+
+// source:
+// https://vuejs.github.io/vetur/guide/setup.html#advanced
+// https://stackoverflow.com/questions/64867504/vue-3-the-template-root-requires-exactly-one-element-eslint-plugin-vue
+
