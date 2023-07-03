6d7c400ab35dfb30c756d74d75665a9c6b9a48a1
Atanu Sarkar <mratanusarkar@gmail.com>
2023-07-03 20:51:08 +0530

    add: new component and move content

--- vue-modal/src/App.vue
+++ vue-modal/src/App.vue
@@ -1,41 +1,16 @@
 <template>
   <!-- Vue 3 now allows us to have multiple elements inside the root template -->
-  <img alt="Vue logo" src="./assets/logo.png">
-  <h1>{{ title }}</h1>
-  <input type="text" ref="name">
-  <button @click="handleButtonClick">click me</button>
-  <h3 @click="handleH3Click">Last Entry: {{ data }}</h3>
+  <Content/>
 </template>
 
 <script>
+import Content from './components/Content.vue';
+
 export default {
   name: 'App',
-  data() {
-    return {
-      title: "Welcome to My First Vue App!",
-      data: "null"
-    }
+  components: {
+    Content
   },
-  methods: {
-    handleButtonClick() {
-      // access the DOM Element via $ref in contrary to the DOM query selector
-      console.log(this.$refs.name);
-      this.data = this.$refs.name.value ? this.$refs.name.value : "null"
-      
-      // add, remove, inject classes dynamically
-      if (this.$refs.name.value) {
-        this.$refs.name.classList.remove('fail')
-        this.$refs.name.classList.add('pass')
-      } else {
-        this.$refs.name.classList.remove('pass')
-        this.$refs.name.classList.add('fail')
-      }
-    },
-    handleH3Click() {
-      // we can also access other normal javascript methods & functions
-      this.$refs.name.focus()
-    }
-  }
 }
 </script>
 
@@ -47,17 +22,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-h1 {
-  border-top: 1px solid #ddd;
-  border-bottom: 1px solid #ddd;
-  padding-top: 10px;
-  padding-bottom: 10px;
-}
-.pass {
-  border: 2px solid green;
-}
-.fail {
-  border: 2px solid red;
 }
 </style>
---
+++ vue-modal/src/components/Content.vue
@@ -0,0 +1,54 @@
+<template>
+  <img alt="Vue logo" src="../assets/logo.png">
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name">
+  <button @click="handleButtonClick">click me</button>
+  <h3 @click="handleH3Click">Last Entry: {{ data }}</h3>
+</template>
+
+<script>
+export default {
+  name: 'Content',
+  data() {
+    return {
+      title: "Welcome to My First Vue App!",
+      data: "null"
+    }
+  },
+  methods: {
+    handleButtonClick() {
+      // access the DOM Element via $ref in contrary to the DOM query selector
+      console.log(this.$refs.name);
+      this.data = this.$refs.name.value ? this.$refs.name.value : "null"
+      
+      // add, remove, inject classes dynamically
+      if (this.$refs.name.value) {
+        this.$refs.name.classList.remove('fail')
+        this.$refs.name.classList.add('pass')
+      } else {
+        this.$refs.name.classList.remove('pass')
+        this.$refs.name.classList.add('fail')
+      }
+    },
+    handleH3Click() {
+      // we can also access other normal javascript methods & functions
+      this.$refs.name.focus()
+    }
+  }
+}
+</script>
+
+<style>
+h1 {
+  border-top: 1px solid #ddd;
+  border-bottom: 1px solid #ddd;
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+.pass {
+  border: 2px solid green;
+}
+.fail {
+  border: 2px solid red;
+}
+</style>
