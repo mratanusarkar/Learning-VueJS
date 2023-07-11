007de9527c56bc340cff694d355aaceaa74fe861
Atanu Sarkar <mratanusarkar@gmail.com>
2023-07-12 00:10:42 +0530

    add: fetch data and show job conditionally

--- vue-05-router/src/App.vue
+++ vue-05-router/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link :to="{ name: 'About' }">About</router-link>
+    <router-link :to="{ name: 'About' }">About</router-link> |
     <router-link :to="{ name: 'Jobs' }">Jobs</router-link>
   </nav>
 
